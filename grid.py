2a2f50ef3794445d7635ba740f1b456b6e8a7748
lukaj <luka.f.jere@gmail.com>
2025-12-27 13:57:09 +0100

    Window popup commit

---
+++ grid.py
@@ -0,0 +1,4 @@
+class Grid:
+    def __init__(self):
+        self.num_rows = 20
+        self.num_cols = 10
--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pygame
+import sys
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+pygame.init()
+
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 700
+dark_blue = (44, 44, 127)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Python tetris version 1")
+clock = pygame.time.Clock()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    #Drawing
+    screen.fill(dark_blue)
+    pygame.display.update()
+    clock.tick(60)
+
