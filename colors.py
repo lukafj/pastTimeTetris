ab5e29d0679bb8d84f69f9f2bb698172735723dc
lukaj <luka.f.jere@gmail.com>
2026-01-04 11:25:06 +0100

    Added sounds, UI and score logic

--- colors.py
+++ colors.py
@@ -8,6 +8,10 @@ class Colors:
     cyan = (21, 204, 209)
     blue = (13, 64, 216)
 
+    white = (255, 255, 255)
+    dark_blue = (44, 44, 127)
+    light_blue = (59, 85, 162)
+
     @classmethod
     def get_cell_colors(cls):
         return [cls.dark_grey, cls.green, cls.red, cls.orange, cls.yellow, cls.purple, cls.cyan, cls.blue]
